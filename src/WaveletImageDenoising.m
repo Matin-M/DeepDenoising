4fe057840b93890c2a8adf5fb1eeb96e47bff6f7
shreeshiv <patelshreeshiv@gmail.com>
2022-11-29 15:37:43 -0700

    Few changes as mentioned

--- src/WaveletImageDenoising.m
+++ src/WaveletImageDenoising.m
@@ -1,15 +1,6 @@
+
 % Wavelet Image Denoising 
+function output = apply_wavelet_denoising(I)
+    output = wdenoise2(I);
+end
 
-img = im2double(rgb2gray(imread('HW04_input.png')));
-noised_image = imnoise(blurredImg,'gaussian',0,powerOfSig/1000);
-denoise_image = wdenoise2(img);
-
-figure; imshow(img);title("Original Image");
-
-subplot(1,2,1)
-imagesc(noised_image)
-title('Noised Image')
-subplot(1,2,2)
-imagesc(denoise_image)
-title('Denoised Image')
-colormap gray
